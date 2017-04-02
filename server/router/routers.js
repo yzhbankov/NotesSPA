9334e27ae9d80d00f520202bf66f339aa93edc5c
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-04-02 23:05:47 +0300

    heroku settings

--- server/router/routers.js
+++ server/router/routers.js
@@ -11,7 +11,7 @@ router.use((req, res, next)=> {
 router.use(express.static(__dirname + '/public'));
 
 router.get('/', (req, res)=> {
-    res.sendfile('public/build/main.bundle.js');
+    res.sendfile('css/style.css');
 });
 
 router
