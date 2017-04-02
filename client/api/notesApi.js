14054f25fa356e11d5c5f4f846f7207616f7c3a9
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-04-03 00:01:50 +0300

    change urls for https://react-flux-notes-app.herokuapp.com/

--- client/api/notesApi.js
+++ client/api/notesApi.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export default {
     listNotes(){
-        return axios.get('http://localhost:8080/notes');
+        return axios.get('https://react-flux-notes-app.herokuapp.com/notes');
     },
     addNote(data){
-        return axios.post('http://localhost:8080/notes', data);
+        return axios.post('https://react-flux-notes-app.herokuapp.com/notes', data);
     },
     deleteNote(id){
-        return axios.delete('http://localhost:8080/notes/' + id)
+        return axios.delete('https://react-flux-notes-app.herokuapp.com/notes/' + id)
     }
 }
